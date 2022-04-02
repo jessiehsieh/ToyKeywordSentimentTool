cbaeea0f035c1f6ecee3d0c6c00d79650f9a5cc5
jessie <jessie.hsiehpm@gmail.com>
2022-04-03 00:33:52 +0200

    updated lambda

--- lambda/call-news-api-new-keyword/lambda_function.py
+++ lambda/call-news-api-new-keyword/lambda_function.py
@@ -9,43 +9,43 @@ LOG = logging.getLogger("handler.py")
 LOG.setLevel(os.environ.get("LOG_LEVEL", logging.DEBUG))
 
 api_key = os.environ['api_key']
-now_str = str(datetime.today()).replace(' ', ',')
+time_now = datetime.now()
+
 
 def lambda_handler(event, context):
-    
-    if event['Records'][0]['eventName']!='MODIFY':
-        response = "Not a modification in records"
-        LOG.info(response)
-        return response
-    
-    LOG.info(event)
-    #key_before = event['Records'][0]['dynamodb']['OldImage']
-    
+
+    event_name = event['Records'][0]['eventName']
+
     if 'NewImage' not in event['Records'][0]['dynamodb']:
         response = "No change in the tracked records."
         LOG.info(response)
         return response
-        
-    key_after = event['Records'][0]['dynamodb']['NewImage']
-    LOG.info(key_after)
     
-    latest_keyphrase = key_after['keyphrase']['S']
-    LOG.info(latest_keyphrase)
-    
-    
-    if key_after['s3_path']['S']=='':
-        
-        LOG.info("""Getting news for keyword - %s"" % (latest_keyphrase)""")
+    if event_name not in ['INSERT', 'MODIFY']:
+        LOG.info(event)
 
+        # {'Records': [{'eventID': '3b1a17999024e18d4ba3e0d760801d03', 'eventName': 'INSERT', 'eventVersion': '1.1', 'eventSource': 'aws:dynamodb', 'awsRegion': 'eu-west-1', 'dynamodb': {'ApproximateCreationDateTime': 1648935792.0, 'Keys': {'keyphrase': {'S': 'example new record'}}, 'NewImage': {'keyphrase': {'S': 'example new record'}}, 'SequenceNumber': '335700000000023132824619', 'SizeBytes': 54, 'StreamViewType': 'NEW_AND_OLD_IMAGES'}, 'eventSourceARN': 'arn:aws:dynamodb:eu-west-1:644211746806:table/keyword-result/stream/2022-04-02T21:10:33.897'}]}
+
+        response = "Not a modification in records"
+        LOG.info(response)
+        return response
+
+    elif event_name == 'MODIFY': 
+	key_before = event['Records'][0]['dynamodb']['OldImage']
+        LOG.info(key_before)
+	old_date_str = key_before['date']['S']
+        old_datetime = datetime.strptime(old_date_str, "%Y-%m-%d")
+	if (time_now - old_datetime).day < 3:
+            response = "Keyword Analysis updated recently"
+            LOG.info(response)
+            return response
+
+    else:
+        key_after = event['Records'][0]['dynamodb']['NewImage']
+        keyphrase = key_after['keyphrase']['S']
 
         url = (f'https://newsapi.org/v2/everything?q={latest_keyphrase}&from=2022-04-02&sortBy=popularity&apiKey={api_key}')
-
         response = requests.get(url)
-
         print(response.json)
-        
         return "response returned"
-    
-    else:
-        
-        return "s3 path exists already"
+   
